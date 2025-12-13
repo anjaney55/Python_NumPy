726163882dd2c9554534faa91f6f97df55805cc4
Anjaney <anjaneynaik5@gmail.com>
2025-12-13 13:07:31 +0530

    initial commit

---
+++ identity_matrix.py
@@ -0,0 +1,6 @@
+## This script creates a 3x3 identity matrix using NumPy and prints it.
+
+import numpy as np
+
+identity_martix = np.eye(3)  # Create a 3x3 identity matrix
+print(identity_martix)  
