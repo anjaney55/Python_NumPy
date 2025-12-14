abf12899e575aac364994f8f746743736fc6f424
Anjaney <anjaneynaik5@gmail.com>
2025-12-14 10:20:47 +0530

    initial commit

---
+++ Aggregation_func.py
@@ -0,0 +1,20 @@
+# Aggregation functions in NumPy
+# These functions are used to perform operations that aggregate values in an array.
+
+
+import numpy as np
+
+arr = np.array([10,20,30,40,50])
+
+print(np.sum(arr))
+
+print(np.mean(arr))
+
+print(np.min(arr))
+
+print(np.max(arr))
+
+print(np.std(arr))
+
+print(np.var(arr))
+
