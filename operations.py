45da7cfd18ba8e977ff05240abc12e643dad7712
Anjaney <anjaneynaik5@gmail.com>
2025-12-16 22:11:35 +0530

    initial commit

---
+++ operations.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr = np.array([10,20,30,40])
+
+print(arr + 5)  # Add 5 to each element
+print(arr - 5)  # Subtract 5 from each element
+print(arr * 2)  # Multiply each element by 2
+print(arr / 3)  # Divide each element by 2
+print(arr ** 2)  # Square each element
+print(arr % 3)  # Modulus operation with 3
+print(arr // 3)  # Floor division by 3
+print(arr > 20)  # Check if each element is greater than 20
+print(arr < 30)  # Check if each element is less than 30
+print(arr == 20)  # Check if each element is equal to 20
+print(arr != 30)  # Check if each element is not equal to 30
