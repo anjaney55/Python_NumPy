1a93b38d707254ca56541e0ae777d791a052711a
Anjaney <anjaneynaik5@gmail.com>
2025-12-19 23:18:12 +0530

    initial commit

---
+++ access.py
@@ -0,0 +1,13 @@
+#Numpy is a zero(0) based index
+# Negative indexing allows you to access elements from the end of the array.
+# Indexing can be used to access single elements or slices of the array.
+# Slicing allows you to access a range of elements in the array.
+# Slicing can be done using the colon (:) operator.
+
+import numpy as np
+
+arr = np.array([23,45,2,78,34])
+
+print(arr[2])
+
+print(arr[-1])
