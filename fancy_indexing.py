581fae3eb4585fd7136cd417553f2b26a406c805
Anjaney <anjaneynaik5@gmail.com>
2025-12-20 11:46:11 +0530

    initial commit

---
+++ fancy_indexing.py
@@ -0,0 +1,11 @@
+# fancy indexing
+
+import numpy as np
+
+arr = np.array([23,45,28,78,34, 7,89])
+
+print(arr[[0,5,2,6]])
+
+'''
+using fancy indexing we can access multiple elements in an array
+It will create the new array'''
